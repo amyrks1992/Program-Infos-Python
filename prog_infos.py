7e2397d74aed2305cf6c61967d0b9ce4c5c12948
Amyr Kamel e Silva <67295939+amyrks1992@users.noreply.github.com>
2020-09-07 00:15:06 -0300

    Add files via upload

---
+++ prog_infos.py
@@ -0,0 +1,4 @@
+nome = input ("Digite o seu nome e aperte enter: ")
+sobrenome = input ("Digite o seu sobrenome e aperte enter: ")
+idade = input ("Digite a sua idade e aperte enter: ")
+print ("O seu nome completo é: ",nome, sobrenome, ", e você tem", idade, "anos")
