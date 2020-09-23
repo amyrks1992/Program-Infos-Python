f0b6267d9773b16fee98b11294843f7eed24ef64
Amyr Kamel e Silva <67295939+amyrks1992@users.noreply.github.com>
2020-09-23 18:38:23 -0300

    Update prog_infos.py

--- prog_infos.py
+++ prog_infos.py
@@ -1,4 +1,4 @@
-nome = input ("Digite o seu nome e aperte enter: ")
-sobrenome = input ("Digite o seu sobrenome e aperte enter: ")
-idade = input ("Digite a sua idade e aperte enter: ")
+nome = input ("Digite o seu nome: ")
+sobrenome = input ("Digite o seu sobrenome: ")
+idade = input ("Digite a sua idade: ")
 print ("O seu nome completo é: ",nome, sobrenome, ", e você tem", idade, "anos")
